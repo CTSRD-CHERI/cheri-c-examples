82939a09178315b50ea9d32ebf0f384a723a1f8e
dave lide <dave@trustedst.com>
2025-12-29 15:38:20 +0000

    updated makefile for ptrdiffxx examples; fixed compile warning for ptrdiffa0 that we did not see with cheribsd compiler version

--- ptrdiffa0/ported-cheri-linux/src/main.c
+++ ptrdiffa0/ported-cheri-linux/src/main.c
@@ -57,7 +57,7 @@ void ctest1( void *b,  ptrdiff_t * new_off)
    intptr_t base;
    base = get_base(b);
    void **p = (void **) (base + *new_off);  
-   *new_off =  ((char*)*p) - base;
+   *new_off = (ptrdiff_t)  ((char*)*p) - base;
    printf("p=%p *p=%p s1=%p  base=%p newoff=%tx\n ", p, *p, s1, (void*) base, *new_off);
 }
 
