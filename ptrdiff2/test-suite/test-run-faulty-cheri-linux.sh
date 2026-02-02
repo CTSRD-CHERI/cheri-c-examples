6e5659a227cbdfd46cf62edd84c8bad76ae19cf6
Mark Valovage <mvalovage@sift.net>
2026-02-02 18:08:23 +0000

    test-run-faulty-cheri-linux.sh

--- ptrdiff2/test-suite/test-run-faulty-cheri-linux.sh
+++ ptrdiff2/test-suite/test-run-faulty-cheri-linux.sh
@@ -1,27 +1,26 @@
 #!/usr/bin/env bash
 
-set -e -o pipefail
+set -o pipefail
 EXAMPLE=$(basename $(cd ../ && pwd))
 NAME="$EXAMPLE-faulty-cheri-linux"
 
 cd ../faulty-cheri-linux/
 
 RUN_RESULTS=$(./build/$EXAMPLE 2>&1)
+status=$?
 
 echo "$RUN_RESULTS"
 
-status=${PIPESTATUS[0]}
-if (( status != 0 )); then
+if (( status == 0 )); then
+    echo "Program ran successfully (should crash)."
     echo "RESULT:  $NAME run failed."
-    exit $status
-fi
-
-if [[ SUCCESS_CONDITION]]; then
-    # Test succeeded
+    exit 1
+elif (( status == 139 )); then
+    echo "CHERI security exception triggered (results in segmentation fault - expected behavior)."
     echo "RESULT:  $NAME run success."
     exit 0
 else
-    # Any other result is a failed test.
+    echo "Runtime failed for unknown reason"
     echo "RESULT:  $NAME run failed."
     exit 1
 fi
