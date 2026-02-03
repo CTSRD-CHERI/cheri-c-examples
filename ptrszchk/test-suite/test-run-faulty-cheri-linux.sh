6202ca05636af8cac75eb5aa588ba9c0332bf01b
Mark Valovage <mvalovage@sift.net>
2026-02-03 22:42:50 +0000

    Faulty cheri linux tests completed.

--- ptrszchk/test-suite/test-run-faulty-cheri-linux.sh
+++ ptrszchk/test-suite/test-run-faulty-cheri-linux.sh
@@ -1,27 +1,28 @@
 #!/usr/bin/env bash
 
-set -e -o pipefail
+set -o pipefail
 EXAMPLE=$(basename $(cd ../ && pwd))
 NAME="$EXAMPLE-faulty-cheri-linux"
 
 cd ../faulty-cheri-linux/
 
 RUN_RESULTS=$(./build/$EXAMPLE 2>&1)
+status=$?
 
 echo "$RUN_RESULTS"
 
-status=${PIPESTATUS[0]}
+# The faulty cheri linux ptrszchk example should fail the run due a failed assertion.
 if (( status != 0 )); then
-    echo "RESULT:  $NAME run failed."
-    exit $status
-fi
-
-if [[ SUCCESS_CONDITION]]; then
-    # Test succeeded
-    echo "RESULT:  $NAME run success."
-    exit 0
+    if grep -Fq "Assertion failed: ret !=0" <<< "$RUN_RESULTS" ; then
+        echo "Program run failed (this is expected for the faulty cheri version)."
+        echo "RESULT:  $NAME run test succeeded."
+        exit 0
+    else
+        echo "Program run failed for unknown reason."
+        exit 1
+    fi
 else
-    # Any other result is a failed test.
+    echo "Program run succeeded (this is a test failure; run should fail for faulty cheri version)."
     echo "RESULT:  $NAME run failed."
     exit 1
 fi
