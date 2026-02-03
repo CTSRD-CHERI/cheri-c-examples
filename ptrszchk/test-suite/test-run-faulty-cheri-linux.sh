33fe8ea763cd8e49e08ae63ae2a82982fcbb1ece
Mark Valovage <mvalovage@sift.net>
2026-02-03 22:31:17 +0000

    Copying template tests.

---
+++ ptrszchk/test-suite/test-run-faulty-cheri-linux.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+EXAMPLE=$(basename $(cd ../ && pwd))
+NAME="$EXAMPLE-faulty-cheri-linux"
+
+cd ../faulty-cheri-linux/
+
+RUN_RESULTS=$(./build/$EXAMPLE 2>&1)
+
+echo "$RUN_RESULTS"
+
+status=${PIPESTATUS[0]}
+if (( status != 0 )); then
+    echo "RESULT:  $NAME run failed."
+    exit $status
+fi
+
+if [[ SUCCESS_CONDITION]]; then
+    # Test succeeded
+    echo "RESULT:  $NAME run success."
+    exit 0
+else
+    # Any other result is a failed test.
+    echo "RESULT:  $NAME run failed."
+    exit 1
+fi
