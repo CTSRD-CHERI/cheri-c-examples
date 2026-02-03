6adc9a1f34196a660420b656e52a1f37e7528146
Mark Valovage <mvalovage@sift.net>
2026-02-03 16:29:30 +0000

    Ported cheri linux tests completed.

--- bndschk/test-suite/test-run-ported-cheri-linux.sh
+++ bndschk/test-suite/test-run-ported-cheri-linux.sh
@@ -16,7 +16,7 @@ if (( status != 0 )); then
     exit $status
 fi
 
-if [[ SUCCESS_CONDITION]]; then
+if grep -Fq "after call to ctest1, res = 0" <<< "$RUN_RESULTS" ; then
     # Test succeeded
     echo "RESULT:  $NAME run success."
     exit 0
