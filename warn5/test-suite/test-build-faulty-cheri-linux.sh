fd2b310d707479f0272f7c15f951a0498ecf0209
Mark Valovage <mvalovage@sift.net>
2026-02-04 04:03:00 +0000

    Faulty cheri tests complete.

--- warn5/test-suite/test-build-faulty-cheri-linux.sh
+++ warn5/test-suite/test-build-faulty-cheri-linux.sh
@@ -6,10 +6,26 @@ NAME="$EXAMPLE-faulty-cheri-linux"
 
 cd ../faulty-cheri-linux/
 
-if make clean; make | tee /dev/tty ; then 
-    echo "RESULT:  $NAME build success." 
-    exit 0
+BUILD_RESULTS=$( { ./make clean; make; } 2>&1)
+status=$?
+
+echo "$BUILD_RESULTS"
+
+# Build should succeed but generate warning
+if (( status == 0 )); then
+    if grep -Fq "warning: incompatible integer to pointer conversion" <<< "$BUILD_RESULTS" && grep -Fq "warning: binary expression on capability types" <<< "$BUILD_RESULTS" ; then
+        echo "Generated expected warnings."
+        echo "RESULT:  $NAME build test succeeded."
+        exit 0
+    else
+        echo "Build did not generate expected warnings."
+        echo "RESULT:  $NAME build test failed."
+        exit 1
+    fi
 else
-    echo "RESULT:  $NAME build failed."
+    echo "Program build failed for unknown reason."
+    echo "RESULT:  $NAME build test failed."
     exit 1
 fi
+
+
--- warn5/test-suite/test-run-faulty-cheri-linux.sh
+++ warn5/test-suite/test-run-faulty-cheri-linux.sh
@@ -16,7 +16,7 @@ if (( status != 0 )); then
     exit $status
 fi
 
-if [[ SUCCESS_CONDITION]]; then
+if grep -Eq "align buf 0x[0-9A-Fa-f]+" <<< "$RUN_RESULTS"  && grep -Eq "done 0x[0-9A-Fa-f]+" <<< "$RUN_RESULTS" ; then
     # Test succeeded
     echo "RESULT:  $NAME run success."
     exit 0
