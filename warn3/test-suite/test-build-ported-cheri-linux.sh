6f20736cf3ade7fb00dee824f64eed1471347ad4
Mark Valovage <mvalovage@sift.net>
2026-02-04 03:01:38 +0000

    Ported cheri linux tests completed.

--- warn3/test-suite/test-build-ported-cheri-linux.sh
+++ warn3/test-suite/test-build-ported-cheri-linux.sh
@@ -6,10 +6,24 @@ NAME="$EXAMPLE-ported-cheri-linux"
 
 cd ../ported-cheri-linux/
 
-if make clean; make | tee /dev/tty ; then 
-    echo "RESULT:  $NAME build success." 
-    exit 0
+BUILD_RESULTS=$( { ./make clean; make; } 2>&1)
+status=$?
+
+echo "$BUILD_RESULTS"
+
+# Build should succeed but generate an integer to ptr cast warning
+if (( status == 0 )); then
+    if ! grep -Fq "warning: binary expression on capability types" <<< "$BUILD_RESULTS" ; then
+        echo "Build did not generate pointer interger comparison warning."
+        echo "RESULT:  $NAME build test succeeded."
+        exit 0
+    else
+        echo "Build generated warning that was supposed to be fixed by ported cheri version."
+        echo "RESULT:  $NAME build test failed."
+        exit 1
+    fi
 else
-    echo "RESULT:  $NAME build failed."
+    echo "Program build failed for unknown reason."
+    echo "RESULT:  $NAME build test failed."
     exit 1
 fi
