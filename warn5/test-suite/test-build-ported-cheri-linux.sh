75a5985c451055ada909814a5fa2531fae47590f
Mark Valovage <mvalovage@sift.net>
2026-02-04 04:07:19 +0000

    Ported cheri testing complete.

--- warn5/test-suite/test-build-ported-cheri-linux.sh
+++ warn5/test-suite/test-build-ported-cheri-linux.sh
@@ -6,10 +6,24 @@ NAME="$EXAMPLE-ported-cheri-linux"
 
 cd ../ported-cheri-linux/
 
-if make clean; make | tee /dev/tty ; then 
-    echo "RESULT:  $NAME build success." 
-    exit 0
+BUILD_RESULTS=$( { ./make clean; make; } 2>&1)
+status=$?
+
+echo "$BUILD_RESULTS"
+
+# Build should succeed but generate warning
+if (( status == 0 )); then
+    if ! grep -Fq "warning: incompatible integer to pointer conversion" <<< "$BUILD_RESULTS" && ! grep -Fq "warning: binary expression on capability types" <<< "$BUILD_RESULTS" ; then
+        echo "Faulty cheri c warnings not generated."
+        echo "RESULT:  $NAME build test succeeded."
+        exit 0
+    else
+        echo "Faulty cheri c warnings were generated."
+        echo "RESULT:  $NAME build test failed."
+        exit 1
+    fi
 else
-    echo "RESULT:  $NAME build failed."
+    echo "Program build failed for unknown reason."
+    echo "RESULT:  $NAME build test failed."
     exit 1
 fi
