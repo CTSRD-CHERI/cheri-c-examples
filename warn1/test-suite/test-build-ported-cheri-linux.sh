c51935a1644508246d81ebdbe147bd1acec2fb5f
Mark Valovage <mvalovage@sift.net>
2026-02-04 00:51:20 +0000

    Ported cheri linux tests completed.

--- warn1/test-suite/test-build-ported-cheri-linux.sh
+++ warn1/test-suite/test-build-ported-cheri-linux.sh
@@ -6,10 +6,24 @@ NAME="$EXAMPLE-ported-cheri-linux"
 
 cd ../ported-cheri-linux/
 
-if make clean; make | tee /dev/tty ; then 
-    echo "RESULT:  $NAME build success." 
-    exit 0
+BUILD_RESULTS=$( { ./make clean; make; } 2>&1)
+status=$?
+
+echo "$BUILD_RESULTS"
+
+# Build should succeed and generate no integer to ptr cast warnings
+if (( status == 0 )); then
+    if grep -Fq "warning: cast from provenance-free integer type to pointer type will give pointer that can not be dereferenced" <<< "$BUILD_RESULTS" ; then
+        echo "Build generated warning that was supposed to be fixed by ported cheri version."
+        echo "RESULT:  $NAME build test failed."
+        exit 1
+    else
+        echo "Build did not generate integer to ptr cast warning."
+        echo "RESULT:  $NAME build test succeeded."
+        exit 0
+    fi
 else
-    echo "RESULT:  $NAME build failed."
+    echo "Program build failed for unknown reason."
+    echo "RESULT:  $NAME build test failed."
     exit 1
 fi
