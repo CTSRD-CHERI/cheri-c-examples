97eff74517a887f54bd9d221496cf2a8f7d34606
Mark Valovage <mvalovage@sift.net>
2026-02-05 10:27:35 -0600

    Baseline x86 tests completed; updated readme; small fix to ported cheri linux test.

--- cap_over_mq/test-suite/test-run-baseline-x86-linux.sh
+++ cap_over_mq/test-suite/test-run-baseline-x86-linux.sh
@@ -16,10 +16,18 @@ if (( status != 0 )); then
     exit $status
 fi
 
-if [[ SUCCESS_CONDITION]]; then
-    # Test succeeded
-    echo "RESULT:  $NAME run success."
-    exit 0
+# Check number of messages sent
+if [[ $(grep -Ec "Message queue  msg [0-9]+ sent successfully" <<< "$RUN_RESULTS") == 10 ]] ; then
+    # Check number of messages received
+    if [[ $(grep -Ec "msg is this is msg [0-9]+" <<< "$RUN_RESULTS") == 10 ]] ; then
+        # Test succeeded
+        echo "RESULT:  $NAME run success."
+        exit 0
+    else
+        echo "Number of sent and received messages don't match."
+        echo "RESULT:  $NAME run failed."
+        exit 1
+    fi
 else
     # Any other result is a failed test.
     echo "RESULT:  $NAME run failed."
