e85885f314cfdf9dd4829b8b25a62bd7024ae685
Mark Valovage <mvalovage@sift.net>
2026-02-05 10:12:02 -0600

    Was receiving 'mq_open: Invalid argument'; Changed message queue to include slash; updated maxmsg and msgsize in line with system defaults.

--- cap_over_mq/baseline-x86-linux/src/main.c
+++ cap_over_mq/baseline-x86-linux/src/main.c
@@ -8,18 +8,18 @@
 
 //cap_over_mq
 
-char msg[16][80];
+char msg[10][80];
 char* msg1;
 int main() {
     mqd_t mq;
     struct mq_attr attr;
 
     attr.mq_flags = 0;
-    attr.mq_maxmsg = 16;
-    attr.mq_msgsize = 16;
+    attr.mq_maxmsg = 10;
+    attr.mq_msgsize = 8;
     attr.mq_curmsgs = 0;
 
-    mq = mq_open("mq", O_CREAT | O_RDWR, 0644, &attr);
+    mq = mq_open("/mq", O_CREAT | O_RDWR, 0644, &attr);
     if (mq == (mqd_t)-1) {
         perror("mq_open");
         return 1;
@@ -27,14 +27,14 @@ int main() {
 
     printf("Message queue opened/created successfully\n");
     int i;
-    for(i=0;i<16;i++) 
+    for(i=0;i<10;i++) 
     {
 
     printf("Message queue  msg %d sent successfully\n",i);
     msg1 = &msg[i][0];
     sprintf(msg1,"this is msg %d",i);
     printf("i=%d\n",i);
-    int r2 =  mq_send(mq,(char*)&msg1, 16,1);
+    int r2 =  mq_send(mq,(char*)&msg1, 8,1);
     printf("send, %d\n", r2);
     if (r2<0) perror("mq_send");
 
@@ -42,7 +42,7 @@ int main() {
     printf("Message queue  msg rx \n");
     char * result;
     unsigned int pri;
-    ssize_t   r =mq_receive(mq, (char *) &result, 16, &pri);
+    ssize_t   r =mq_receive(mq, (char *) &result, 8, &pri);
     if (r<0) perror("recv");
     //printf("got a result, %#p %d\n",result,r);
     printf(" msg is %s\n", result);
@@ -54,7 +54,7 @@ int main() {
     }
 
     // Unlink the message queue (optional, to remove it from the system)
-    if (mq_unlink("mq") == -1) {
+    if (mq_unlink("/mq") == -1) {
         perror("mq_unlink");
         return 1;
     }
