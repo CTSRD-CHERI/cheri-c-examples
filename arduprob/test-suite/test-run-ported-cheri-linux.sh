c09e9e3035227070be5323a168deba44575f6358
Mark Valovage <mvalovage@sift.net>
2026-02-05 19:29:43 +0000

    Updating ported cheri linux test to test for 5 lines of output; faulty cheri linux tests complete.

--- arduprob/test-suite/test-run-ported-cheri-linux.sh
+++ arduprob/test-suite/test-run-ported-cheri-linux.sh
@@ -17,10 +17,16 @@ if (( status != 0 )); then
 fi
 
 # Check for first and last lines of output
-if grep -Fq "base=" <<< "$RUN_RESULTS" && grep -Fq "param_b1 addr" <<< "$RUN_RESULTS"; then
+if grep -Fq "base=" <<< "$RUN_RESULTS" && grep -Eq "param_a1 addr.+val 11" <<< "$RUN_RESULTS" && grep -Eq "param_a2 addr.+val 22" <<< "$RUN_RESULTS"; then
+    if grep -Eq "param_class_b addr.+val =0x[0-9A-Fa-f]+" <<< "$RUN_RESULTS" && grep -Eq "param_b1 addr.+val =33" <<< "$RUN_RESULTS"; then
         # Test succeeded
         echo "RESULT:  $NAME run success."
         exit 0
+    else
+        # Any other result is a failed test.
+        echo "RESULT:  $NAME run failed."
+        exit 1
+    fi
 else
     # Any other result is a failed test.
     echo "RESULT:  $NAME run failed."
