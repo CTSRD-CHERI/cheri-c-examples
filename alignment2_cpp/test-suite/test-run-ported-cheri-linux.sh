e55f9c8f6ba0a9db45e856b5bfe2bf241da9d06e
Mark Valovage <mvalovage@sift.net>
2026-02-05 18:31:06 +0000

    Ported cheri tests completed.

--- alignment2_cpp/test-suite/test-run-ported-cheri-linux.sh
+++ alignment2_cpp/test-suite/test-run-ported-cheri-linux.sh
@@ -16,10 +16,19 @@ if (( status != 0 )); then
     exit $status
 fi
 
-if [[ SUCCESS_CONDITION]]; then
-    # Test succeeded
-    echo "RESULT:  $NAME run success."
-    exit 0
+REGEXP="[0-9A-Fa-f]+"
+
+# Test for first line.
+if grep -Eq "ctest1: pointer-casts requires proper alignment a=0x$REGEXP .+ .da=$REGEXP" <<< "$RUN_RESULTS"; then
+    # Test for second line.
+    if grep -Eq "ctest1, v=0x$REGEXP.+blah = 0x$REGEXP" <<< "$RUN_RESULTS"; then
+        # Test succeeded
+        echo "RESULT:  $NAME run success."
+        exit 0
+    else
+        echo "RESULT:  $NAME run failed."
+        exit 1
+    fi
 else
     # Any other result is a failed test.
     echo "RESULT:  $NAME run failed."
