7e42a4c740c64eb3c8df223d4a2e008dd6aa52d0
Mark Valovage <mvalovage@sift.net>
2026-02-05 15:02:02 -0600

    capasint_cpp baseline x86 tests complete.

--- capasint_cpp/test-suite/test-run-baseline-x86-linux.sh
+++ capasint_cpp/test-suite/test-run-baseline-x86-linux.sh
@@ -16,7 +16,7 @@ if (( status != 0 )); then
     exit $status
 fi
 
-if [[ SUCCESS_CONDITION]]; then
+if grep -Fq "capasint, array[0] = 99" <<< "$RUN_RESULTS" ; then
     # Test succeeded
     echo "RESULT:  $NAME run success."
     exit 0
