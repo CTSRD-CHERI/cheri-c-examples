e8f5dcb71fa2da343bf1c9415a02ebf370b72949
Mark Valovage <mvalovage@sift.net>
2026-02-09 20:34:31 +0000

    Updating alingment2 faulty cheri linux test to detect bus error in line with expected behavior.

--- alignment2/test-suite/test-run-faulty-cheri-linux.sh
+++ alignment2/test-suite/test-run-faulty-cheri-linux.sh
@@ -1,27 +1,26 @@
 #!/usr/bin/env bash
 
-set -e -o pipefail
+set -o pipefail
 EXAMPLE=$(basename $(cd ../ && pwd))
 NAME="$EXAMPLE-faulty-cheri-linux"
 
 cd ../faulty-cheri-linux/
 
 RUN_RESULTS=$(./build/$EXAMPLE 2>&1)
+status=$?
 
 echo "$RUN_RESULTS"
 
-status=${PIPESTATUS[0]}
-if (( status != 0 )); then
+if (( status == 0 )); then
+    echo "Program ran successfully (should crash)."
     echo "RESULT:  $NAME run failed."
-    exit $status
-fi
-
-if grep -Eq "ctest1, v=0x[0-9A-Fa-f]+ &blah = 0x[0-9A-Fa-f]+ Passes" <<< "$RUN_RESULTS" ; then
-    # Test succeeded
+    exit 1
+elif (( status == 135 )); then
+    echo "CHERI security exception triggered (results in bus error - expected behavior)."
     echo "RESULT:  $NAME run success."
     exit 0
 else
-    # Any other result is a failed test.
+    echo "Runtime failed for unknown reason"
     echo "RESULT:  $NAME run failed."
     exit 1
 fi
