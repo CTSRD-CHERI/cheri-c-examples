f065a1aac9ff7265c5e046117aa537c8d1c6661d
User Mvalovage <mvalovage@cheribsd1.sift.net>
2026-02-20 03:07:48 +0000

    readplus1 cheri bsd faulty and ported examples complete along with tests.

---
+++ readplus1/test-suite/test-run-faulty-cheri-bsd.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -o pipefail
+EXAMPLE=$(basename $(cd ../ && pwd))
+NAME="$EXAMPLE-faulty-cheri-bsd"
+
+cd ../faulty-cheri-bsd/
+
+RUN_RESULTS=$(./build/$EXAMPLE 2>&1)
+status=$?
+
+echo "$RUN_RESULTS"
+
+if [ "$status" -eq 0 ]; then
+    echo "Program ran successfully (should crash)."
+    echo "RESULT:  $NAME run failed."
+    exit 1
+elif [ "$status" -eq 162 ]; then
+    echo "CHERI security exception triggered (results in signal error - expected behavior)."
+    echo "RESULT:  $NAME run success."
+    exit 0
+else
+    echo "Runtime failed for unknown reason"
+    echo "RESULT:  $NAME run failed."
+    exit 1
+fi
