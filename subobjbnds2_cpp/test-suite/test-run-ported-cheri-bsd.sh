be899034db23a2e797fe1a597980737ef08266c6
User Mvalovage <mvalovage@cheribsd1.sift.net>
2026-02-20 15:55:56 +0000

    subobjbnds2_cpp cheri bsd faulty and ported examples completed along with tests.

---
+++ subobjbnds2_cpp/test-suite/test-run-ported-cheri-bsd.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e -o pipefail
+EXAMPLE=$(basename $(cd ../ && pwd))
+NAME="$EXAMPLE-ported-cheri-bsd"
+
+cd ../ported-cheri-bsd/
+
+RUN_RESULTS=$(./build/$EXAMPLE 2>&1)
+status=$?
+
+echo "$RUN_RESULTS"
+
+if [ "$status" -ne 0 ]; then
+    echo "RESULT:  $NAME run failed."
+    exit $status
+fi
+
+count=$(printf '%s\n' "$RUN_RESULTS" | grep -Ec 'val [0-9][0-9]* = 0')
+
+if [ "$count" -eq 25 ]; then
+    # Test succeeded
+    echo "RESULT:  $NAME run success."
+    exit 0
+else
+    # Any other result is a failed test.
+    echo "RESULT:  $NAME run failed."
+    exit 1
+fi
