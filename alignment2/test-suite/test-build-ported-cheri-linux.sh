15ee0dc937023011c2ccd72d805798a50d232434
Mark Valovage <mvalovage@sift.net>
2026-02-26 22:01:56 -0600

    Updating test-build-*.sh scripts to capture warning messages in log file.

--- alignment2/test-suite/test-build-ported-cheri-linux.sh
+++ alignment2/test-suite/test-build-ported-cheri-linux.sh
@@ -6,7 +6,7 @@ NAME="$EXAMPLE-ported-cheri-linux"
 
 cd ../ported-cheri-linux/
 
-if make clean; make | tee -a /dev/tty ; then 
+if make clean && make 2>&1 | tee -a /dev/tty ; then 
     echo "RESULT:  $NAME build success." 
     exit 0
 else
--- capasint_cpp/test-suite/test-build-faulty-cheri-bsd.sh
+++ capasint_cpp/test-suite/test-build-faulty-cheri-bsd.sh
@@ -6,7 +6,7 @@ NAME="$EXAMPLE-faulty-cheri-bsd"
 
 cd ../faulty-cheri-bsd/
 
-if make clean; make | tee -a /dev/tty ; then 
+if make clean && make 2>&1 | tee -a /dev/tty ; then 
     echo "RESULT:  $NAME build success." 
     exit 0
 else
