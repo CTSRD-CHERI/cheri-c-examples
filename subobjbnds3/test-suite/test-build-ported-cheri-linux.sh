ce6cad169518293b8f24a19b7e603255bda2b4f9
Mark Valovage <mvalovage@sift.net>
2026-05-02 03:35:09 +0000

    Updating ported cheri linux tests to check for purecap.

--- subobjbnds3/test-suite/test-build-ported-cheri-linux.sh
+++ subobjbnds3/test-suite/test-build-ported-cheri-linux.sh
@@ -6,9 +6,21 @@ NAME="$EXAMPLE-ported-cheri-linux"
 
 cd ../ported-cheri-linux/
 
-if make clean && make 2>&1 | { [ -t 1 ] && tee -a /dev/tty || cat; }; then 
-    echo "RESULT:  $NAME build success." 
-    exit 0
+BUILD_RESULTS="$(make clean && make 2>&1)"
+STATUS=$?
+
+echo "$BUILD_RESULTS"
+
+if [ "$STATUS" -eq 0 ]; then
+    if grep -Fq -- "-march=morello" <<< "$BUILD_RESULTS" \
+    && grep -Fq -- "-mabi=purecap" <<< "$BUILD_RESULTS" \
+    && grep -Fq -- "--target=aarch64-linux-musl_purecap" <<< "$BUILD_RESULTS"; then
+        echo "RESULT:  $NAME build success." 
+        exit 0
+    else
+        echo "RESULT:  $NAME build failed.  Purecap not used during compile"
+        exit 1
+    fi
 else
     echo "RESULT:  $NAME build failed."
     exit 1
