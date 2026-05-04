360ab0f4c2296e0581962612a77dac28c4264696
Mark Valovage <mvalovage@sift.net>
2026-05-04 19:26:53 +0000

    Adding purecap flag checks to cheribsd build tests.

--- capasint_cpp/test-suite/test-build-faulty-cheri-bsd.sh
+++ capasint_cpp/test-suite/test-build-faulty-cheri-bsd.sh
@@ -6,9 +6,21 @@ NAME="$EXAMPLE-faulty-cheri-bsd"
 
 cd ../faulty-cheri-bsd/
 
-if make clean && make 2>&1 | { [ -t 1 ] && tee -a /dev/tty || cat; }; then 
-    echo "RESULT:  $NAME build success." 
-    exit 0
+BUILD_RESULTS="$(make clean && make 2>&1)"
+STATUS=$?
+
+echo "$BUILD_RESULTS"
+
+if [ "$STATUS" -eq 0 ]; then
+    if printf '%s\n' "$BUILD_RESULTS" | grep -Fq -- "-march=morello" \
+    && printf '%s\n' "$BUILD_RESULTS" | grep -Fq -- "-mabi=purecap" \
+    && printf '%s\n' "$BUILD_RESULTS" | grep -Fq -- "--target=aarch64-linux-musl_purecap" ; then
+        echo "RESULT:  $NAME build success." 
+        exit 0
+    else
+        echo "RESULT:  $NAME build failed.  Purecap not used during compile"
+        exit 1
+    fi
 else
     echo "RESULT:  $NAME build failed."
     exit 1
